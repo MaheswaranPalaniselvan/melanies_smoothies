0a7395e12eba2d2772d3fa02ce3753ba6405d147
MaheswaranPalaniselvan <79225498+MaheswaranPalaniselvan@users.noreply.github.com>
2025-11-02 21:58:37 +0530

    Update smoothie app to fetch and display fruit data
    Refactor smoothie ingredient selection and display nutrition information for each fruit chosen.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -39,6 +37,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st.text(smoothiefroot_response.json())
+        sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.text(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
